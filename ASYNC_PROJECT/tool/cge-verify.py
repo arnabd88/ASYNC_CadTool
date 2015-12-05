abfc1d1a15fda10cb3b9241a2ab27e2b5f452644
arnabd88 <arnab.d.88@gmail.com>
2015-12-05 13:11:20 -0700

    Projected space completed

--- ASYNC_PROJECT/tool/cge-verify.py
+++ ASYNC_PROJECT/tool/cge-verify.py
@@ -8,6 +8,7 @@ flpn = open(sys.argv[1], 'r+');
 fckt = open(sys.argv[2], 'r+');
 fsg  = open(sys.argv[1].split('.')[0]+'.sg', 'w')
 
+
 lpn = []
 ckt = []
 
@@ -24,12 +25,16 @@ SG = graph.graph(graph_name[0])
 SG.CreateDSlpn(lpn)
 getSG = SG.find_SG()
 print "Message :", getSG[0]
-print "\n\n\n\n"
-print "=============== SET ================"
-print getSG[1] ,"\n\n"
-print "=============== DELTA(M,t,Mi) =============="
-print getSG[2], "\n\n"
-print "=============== LambdaS ================"
-print getSG[3]
-func.writeSG(fsg, getSG)
+if(len(getSG)>1):
+	print "\n\n\n\n"
+	print "=============== SET ================"
+	print getSG[1] ,"\n\n"
+	print "=============== DELTA(M,t,Mi) =============="
+	print getSG[2], "\n\n"
+	print "=============== LambdaS ================"
+	print getSG[3]
+	func.writeSGfull(fsg, getSG)
+	if(len(getSG[6]) !=0 ):
+		fsgSpec = open(sys.argv[1].split('.')[0]+'.spSpec', 'w')
+		func.writeSGspec(fsgSpec, getSG)
 #SG.disPlay()
--- ASYNC_PROJECT/tool/func.py
+++ ASYNC_PROJECT/tool/func.py
@@ -9,8 +9,53 @@ def trimleft( trimString ):
 		trimString = trimString[1:]
 	return trimString
 	
+def writeSGspec( fsgSpec, sglist ):
+	##--------- Writes the SG spec output file ---------
+	SET     = copy.deepcopy(sglist[1])
+	DELTA   = copy.deepcopy(sglist[2])
+	LAMBDAS = copy.deepcopy(sglist[3])
+	signalList = sglist[4]+sglist[5] 
+	print "Signals :", signalList
 	
-def writeSG( fsg, sglist ):
+	fsgSpec.write("Inputs :"+str(sglist[4])+"\n")
+	fsgSpec.write("Outputs :"+str(sglist[5])+"\n")
+	fsgSpec.write("Signal Vector : <"+str(signalList)+">\n\n\n")
+	fsgSpec.write(".SG\n\n")
+	
+	DELTA_MOD = []
+	
+	for i in DELTA:
+		tx = copy.deepcopy(i[1])
+		if('+' in tx):
+			signal = tx.split('+')[0]
+		elif('-' in tx):
+			signal = tx.split('-')[0]
+		if(signal in signalList):
+			DELTA_MOD.append(i)
+	
+	trackDuplicate = []
+	
+	for i in DELTA_MOD:
+		tx        =  copy.deepcopy(i[1])
+		from_Mark = copy.deepcopy(i[0])
+		to_Mark   = copy.deepcopy(i[2])
+		fromState = ""
+		toState   = ""
+		for j in signalList:
+			fromState = fromState+LAMBDAS[from_Mark][j]
+			
+		for k in signalList:
+			toState   = toState+LAMBDAS[to_Mark][k]
+			
+		if ([fromState,tx,toState] not in trackDuplicate):
+			trackDuplicate.append([fromState,tx,toState])			
+			#fsg.write(fromState+"----"+tx+"----"+toState+"\n")
+			##fsg.write('{0:7d} {1:7d} {2:7d}' .format(fromState, tx, toState))
+			fsgSpec.write('{0:7} {1:8} {2:7}' .format(fromState, tx, toState))
+			fsgSpec.write('\n')
+	fsgSpec.write("\n.end")
+	
+def writeSGfull( fsg, sglist ):
 	##---------- Writes the SG output file --------------
 	SET     = copy.deepcopy(sglist[1])
 	DELTA   = copy.deepcopy(sglist[2])
@@ -36,7 +81,11 @@ def writeSG( fsg, sglist ):
 		for k in signalList:
 			toState   = toState+LAMBDAS[to_Mark][k]
 			
-		fsg.write(fromState+"----"+tx+"----"+toState+"\n")
+		#fsg.write(fromState+"----"+tx+"----"+toState+"\n")
+		##fsg.write('{0:7d} {1:7d} {2:7d}' .format(fromState, tx, toState))
+		fsg.write('{0:7} {1:8} {2:7}' .format(fromState, tx, toState))
+		fsg.write('\n')
+
 		
 	fsg.write("\n.end")
 		
